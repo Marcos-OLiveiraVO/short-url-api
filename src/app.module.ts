17b5c1c657dca85c093c3d4345c0f1eb73b05b5d
Marcos-OLiveiraVO <marcosoliveira.rd@gmail.com>
2025-07-29 00:52:02 -0400

    Feat - added swagger doc config and added profile module to app module.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
+import { ProfileModule } from '@profile/profile.module';
 import { DatabaseModule } from '@shared/database/database.module';
 import { AuthenticationGuard } from '@shared/middleware/auth/authentication.guard';
 import { AuthenticationModule } from '@shared/middleware/auth/authentication.module';
 
 @Module({
   providers: [{ provide: APP_GUARD, useClass: AuthenticationGuard }],
-  imports: [DatabaseModule, AuthenticationModule],
+  imports: [DatabaseModule, AuthenticationModule, ProfileModule],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,22 @@ async function bootstrap() {
       transformOptions: { enableImplicitConversion: true },
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Short URL API')
+    .setContact('Marcos Oliveira', 'https://www.linkedin.com/in/marcos-oliveiraaa/', 'marcosoliveira.rd@gmail.com')
+    .setDescription('The Short URL API description')
+    .setVersion('0.6.0')
+    .addBearerAuth({
+      description: 'Please enter token in following format: Bearer <JWT>',
+      name: 'Authorization',
+      type: 'http',
+      bearerFormat: 'Bearer',
+    })
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   await app.listen(3000);
 }
